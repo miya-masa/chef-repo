58a036289dc57fca4ea24f3f9856aa2ca47ada1d
miya-masa <miyauchi0623@gmail.com>
2014-12-18 09:37:14 +0000

    commit!

---
+++ chef-repo/site-cookbooks/vim/attributes/default.rb
@@ -0,0 +1,1 @@
+default['vim']['version']='7.4'
--- chef-repo/site-cookbooks/vim/recipes/default.rb
+++ chef-repo/site-cookbooks/vim/recipes/default.rb
@@ -6,13 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
-bash "vim install" do
+vim_version = node['vim']['version']
+remote_file "/tmp/vim-#{vim_version}.tar.bz2" do
+  source "http://ftp.vim.org/pub/vim/unix/vim-#{vim_version}.tar.bz2"
+  not_if "::File.exists?(/usr/local/bin/vim)"
+  notifies :run, "bash[install_vim]", :immediately
+end
+bash "install_vim" do
   cwd "/tmp/"
   code <<-EOH 
-  wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2 
-  tar -jvxf vim-7.4.tar.bz2
-  cd vim74
+  mkdir vim-#{vim_version}
+  tar -jvxf vim-#{vim_version}.tar.bz2 -C vim-#{vim_version} --strip-components 1
+  cd vim-#{vim_version}
   ./configure
   make 
   make install
@@ -20,21 +25,22 @@ bash "vim install" do
 end
 
 
-# SCM Resource, Chef::Provider::Git
-git "/home/vagrant/dotfiles/" do # Path to clone/checkout/export the source to
-  repository "https://github.com/miya-masa/dotfiles.git" # URI of the repository
-  user "vagrant"
-  group "vagrant"
-  revision "master" # revision to checkout. can be symbolic, like "HEAD" or an SCM specific revision id
-  action :sync # Update the source to the specified revision, or get a new clone (Default)
-end
-
-bash "dotfiles.sh" do
-  cwd "/home/vagrant/dotfiles/"
-  user "vagrant"
-  group "vagrant"
-  code <<-EOH 
-  chmod 744 dotFilesLink.sh
-  sudo -u vagrant ./dotFilesLink.sh
-  EOH
-end
+## SCM Resource, Chef::Provider::Git
+#git "/home/vagrant/dotfiles/" do # Path to clone/checkout/export the source to
+#  repository "https://github.com/miya-masa/dotfiles.git" # URI of the repository
+#  user "vagrant"
+#  group "vagrant"
+#  revision "master" # revision to checkout. can be symbolic, like "HEAD" or an SCM specific revision id
+#  action :sync # Update the source to the specified revision, or get a new clone (Default)
+#end
+#
+#bash "dotfiles.sh" do
+#  cwd "/home/vagrant/dotfiles/"
+#  user "vagrant"
+#  group "vagrant"
+#  code <<-EOH 
+#  chmod 744 dotFilesLink.sh
+#  su - vagrant 
+#  bash ./dotFilesLink.sh
+#  EOH
+#end
